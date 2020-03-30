b0c620628d043a3accd4d24e8b8c362bc58b8d5f
kaanayvaz5 <50020678+kaanayvaz5@users.noreply.github.com>
2020-03-30 23:04:12 +0200

    Revert "Revert "Score+Timer""
    This reverts commit 4e8c6ea65d6081050d578ed464dca22500cfa6d5.

---
+++ Assets/Scriptas/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Timer : MonoBehaviour
+{
+    public float currentTime;
+    public float startTime;
+    public Text timerText;
+    
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        int splitSec = (int)(currentTime * 100) % 100;
+        int seconds = (int)(currentTime % 60);
+        int minutes = (int)(currentTime / 60);
+
+        string timerStr = string.Format("{0:00}:{1:00}.{2:00}", minutes, seconds, splitSec);
+
+        timerText.text = timerStr;
+    }
+}
